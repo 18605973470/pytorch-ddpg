f60579c43759ac9e53efc4f0e93c1fa272f67caf
fanyang <yf314043711@gmail.com>
2018-12-02 10:12:55 +0800

    modify models

--- models.py
+++ models.py
@@ -5,28 +5,6 @@ from torch import nn
 import torch.nn.functional as F
 
 
-class ValueNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
-        super(ValueNetwork, self).__init__()
-
-        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
-
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear3 = nn.Linear(hidden_size, 1)
-
-        nn.init.xavier_uniform_(self.linear1.weight)
-        nn.init.xavier_uniform_(self.linear2.weight)
-        self.linear3.weight.data.uniform_(-init_w, init_w)
-        self.linear3.bias.data.uniform_(-init_w, init_w)
-
-    def forward(self, state, action):
-        x = torch.cat([state, action], 1)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
-        x = self.linear3(x)
-        return x
-
-
 class PolicyNetwork(nn.Module):
     def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
         super(PolicyNetwork, self).__init__()
@@ -43,8 +21,36 @@ class PolicyNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.linear1(state))
         x = F.relu(self.linear2(x))
+
+        feature_vector = x
+
         x = F.softsign(self.linear3(x))
+        return x, feature_vector
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, feature_vector_dim, num_actions, hidden_size, init_w=3e-3):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(feature_vector_dim + num_actions, hidden_size)
+
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+
+        nn.init.xavier_uniform_(self.linear1.weight)
+        nn.init.xavier_uniform_(self.linear2.weight)
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, feature_vector, action):
+        x = torch.cat([feature_vector, action], 1)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
         return x
 
+
+# class CommonNetwork(nn.Module):
+#     def __init__(self):
 # class ActorCriticNetwork(nn.Module):
 #     def __init__(self, num_inputs, num_actions):
